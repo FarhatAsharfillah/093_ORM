b81fd47a6ebec99609e965329d6b9594e1ac96e3
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-27 10:00:31 +0700

    menambahkan kode CRUD di dalam index js menggunakan post, get, put, delete

--- index.js
+++ index.js
@@ -20,3 +20,54 @@ db.sequelize.sync()
     .catch((err) => {
         console.error('Failed to sync database:', err);
     });
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
+        await komik.update(data);
+        res.send({ message: 'Komik updated successfully', komik });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
+        await komik.destroy();
+        res.send({ message: 'Komik deleted successfully' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
