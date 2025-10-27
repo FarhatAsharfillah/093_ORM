987d892731d2dcf0261d69e4b64feec0b3e4cfab
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-27 09:26:56 +0700

    menambahkan kode di dalam index js seperti app.listen dan db.sequelize

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const PORT = 3309;
-const db = require ("./models");
+const PORT = 3000;
+const db = require('./models');
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
+app.use(express.urlencoded({ 
+    extended: true 
 }));
 
+app.listen(PORT, async () => {
+    console.log(`Server is running on port 3000`);
+});
+
+db.sequelize.sync()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Database synced and server is running on port 3000');
+        })
+    })
+    .catch((err) => {
+        console.error('Failed to sync database:', err);
+    });
